06218ef93cbe7967928a45d66a065f75574174e4
Stefan Butacu <stefbutacu@gmail.com>
2023-05-20 15:05:07 +0300

    Renaming loss

--- model/losses/ce_iou_loss.py
+++ model/losses/ce_iou_loss.py
@@ -6,9 +6,9 @@ from torch.nn import CrossEntropyLoss
 from model.losses.iou_loss import IoULoss
 
 
-class CE_DICE_Loss(nn.Module):
+class CE_IOU_Loss(nn.Module):
     def __init__(self, alpha=0.5):
-        super(CE_DICE_Loss, self).__init__()
+        super(CE_IOU_Loss, self).__init__()
         self.alpha = alpha
         self.cross_entropy_loss = CrossEntropyLoss()
         self.iou_loss = IoULoss()
